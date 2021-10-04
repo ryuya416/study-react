83c7f795f298d33f55617aa5254fd0dc2b78d8fe
ryuya416 <h2luv0r1a8@icloud.com>
2021-10-04 18:22:28 +0900

    ⚡️ コメントの個別ページをSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,11 +1,48 @@
 import { CommentComponent } from "src/components/Comment";
 import { Header } from "src/components/Header";
+import { SWRConfig } from "swr";
 
-const CommentsId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch(
+    "https://jsonplaceholder.typicode.com/comments/"
+  );
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: {
+      id: comment.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentsId = (props) => {
+  const { fallback } = props;
+
   return (
     <div>
       <Header />
-      <CommentComponent />
+      <SWRConfig value={{ fallback }}>
+        <CommentComponent />
+      </SWRConfig>
     </div>
   );
 };
